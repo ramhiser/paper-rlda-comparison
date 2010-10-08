a1ea2e43551de400b41688325772c0e39c86f156
John Ramey <johnramey@gmail.com>
2010-10-08 14:05:51 -0500

    Variable selection with t-test works now. Only works for 2 classes via t-test. Need to implement ANOVA option.

--- variable-selection.r
+++ variable-selection.r
@@ -1,13 +1,21 @@
 library(plyr)
 
-variable.selection.t.test <- function(training.df, alpha = 0.01) {
-	var.select.pvals <- daply(training.df, 2, 
+# For each variable in the data frame, we test the hypothesis that the means are
+# equal for each class and calculate the p-value.
+# The variables that yield p-values < alpha are kept.
+# The variables that yield p-values >= alpha are dropped.
+# Assumes the first column contains the class (population) labels.
+variable.selection.t.test <- function(df, alpha = 0.01) {
+	var.select.pvals <- aaply(df[,-1], 2, 
 		function(col) {
-			col.by.class <- split(col, temp.colon.labels)
+			col.by.class <- split(col, df[,1])
 			p.val <- t.test(col.by.class[[1]], col.by.class[[2]])$p.value
 		})
+		
+	kept.variables <- which(var.select.pvals < alpha)
+	dropped.variables <- which(var.select.pvals >= alpha)
+	
+	# We include the first column, which contains the original labels, and the variables with p-val < alpha.
+	subset.df <- df[, c(1, kept.variables)]
+	list(subset.df = subset.df, kept.variables = kept.variables, dropped.variables, p.vals = var.select.pvals)
 }
-
-
-filtered.x <- colon.x[, var.select.pvals < alpha]
-transformed.x <- transformed.colon.train[, var.select.pvals < alpha]
