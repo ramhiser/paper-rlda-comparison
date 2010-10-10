976259d77beea17657ad4249ccb19b0a7d564065
John Ramey <johnramey@gmail.com>
2010-10-09 20:43:13 -0500

    Added source() calls to local files for simulation: leave-k-out.r and variable-selection.r

--- sim-colon.r
+++ sim-colon.r
@@ -3,6 +3,9 @@ library(corpcor) # for cov.shrink and inv.cov.shrink for MLDA
 library(plyr)
 library(MASS)
 
+source("leave-k-out.r")
+source("variable-selection.r")
+
 # Temporarily we have to manually run the source code from the RLDA project.
 # We are using a rlda.dir because the directory on my Mac differs from the RLDA directory on the Baylor cluster.
 rlda.dir <- "~/Dropbox/R/rlda/R/"
